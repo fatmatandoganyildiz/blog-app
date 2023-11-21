651b82e679bf82b851a316c4de4b7fd04db16d15
Fatma Tandogan Yildiz <fatmatandogan609@gmail.com>
2023-11-21 11:06:49 +0000

    Article Page/ Account Page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
 
 function Account() {
   return (
-    <div>Account</div>
-  )
+    <div className=" bg-red-200">
+      <form className="mx-14 my-12">
+        <h1 className="text-2xl font-light font-serif tracking-widest text-center py-2">
+          Your Articles
+        </h1>
+        {/* <div>
+            <h2 className="text-xl font-light font-serif tracking-wider py-2">
+              Choose an Section
+            </h2>
+            <ul className="px-1">
+              <li>
+                <input type="checkbox" />
+                Life-Style
+              </li>
+              <li>
+                {" "}
+                <input type="checkbox" />
+                Travel
+              </li>
+              <li>
+                {" "}
+                <input type="checkbox" />
+                Health
+              </li>
+            </ul>
+          </div> */}
+        <div className="flex flex-col items-center bg-blue-400">
+          <h2 className="text-2xl font-light font-serif tracking-wider py-2">
+            Tell Your Story
+          </h2>
+          <input
+            placeholder="Title"
+            type="text"
+            className="border-l outline-none p-2 mb-2 text-xl font-medium"
+          />
+          <input placeholder="Your Story"  type="text" className="outline-none p-2 mx-9" />
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Account
+export default Account;
--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,62 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { key } from "../request";
-import { UserAuth } from "../context/AuthContext";
-import { db } from "../firebase";
-import { arrayUnion, doc, updateDoc } from "firebase/firestore";
 
-function Article({item}) {
-  const [like, setLike] = useState(false);
-  const [saved, setSaved] = useState(false);
-  const { user } = UserAuth();
-
-  let params = useParams();
-
-  const articleID = doc(db, "users", `${user?.email}`);
-
-  const saveArticle = async () => {
-    if (user?.email) {
-      setLike(!like);
-      setSaved(true);
-      await updateDoc(articleID, {
-        saveArticle: arrayUnion({
-          id: item.id,
-          title: item.title,
-          // img: item.backdrop_path,//
-        }),
-      });
-    } else {
-    }
-  };
-
+function Article() {
+  const { name } = useParams();
   const [details, setDetails] = useState({});
 
   useEffect(() => {
-    // setDetails({});
-    if (params.name) {
+    if (name) {
       const fetchDetails = async () => {
-        const data = await fetch(
-          `https://api.unsplash.com/photos/query=${params.name}&client_id=${key}
-          `
-        );
-        const detailData = await data.json();
-        console.log(detailData);
-        setDetails(detailData);
+        try {
+          const response = await fetch(
+            `https://api.unsplash.com/photos/${name}?client_id=${key}`
+          );
+
+          if (!response.ok) {
+            throw new Error("Photo not found");
+          }
+          const detailData = await response.json();
+          console.log(detailData);
+          setDetails(detailData);
+        } catch (error) {
+          console.error("Error fetching photo details:", error.message);
+        }
       };
+
       fetchDetails();
     }
-  }, [params.name]);
+  }, [name]);
 
   return (
     <div>
-      <img
-        key={details.current_user_collections}
-        className="w-[560px] h-[200px] border object-cover "
-        src={`${details.urls}`}
-        alt="alt"
-      />
-      {/* <h4 className="text-2xl my-2" key={details.id}>{`${details.current_user_collections.description}`}</h4>  */}
-      <p className="ml-1">{`${details}`}</p>
+      <p>Photo Name: {details?.alt_description}</p>
     </div>
   );
 }
